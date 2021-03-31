25e6c497a373f45fa18fa37094ae63a187f9692f
KAKEI Kazuki <kkikzk@gmail.com>
2021-03-31 21:06:15 +0900

    プロジェクトを追加

---
+++ Sha1SampleCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sha1SampleCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
