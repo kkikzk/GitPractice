03845bc5912ee79b957aef838439bd147c2f58b6
KAKEI Kazuki <kkikzk@gmail.com>
2021-03-31 21:54:59 +0900

    コード修正

--- Sha1SampleCode/Program.cs
+++ Sha1SampleCode/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Sha1SampleCode
 {
@@ -6,7 +9,28 @@ namespace Sha1SampleCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sep = Path.DirectorySeparatorChar;
+            var path = "." + sep + ".." + sep + ".." + sep + ".." + sep + ".." + sep + "hello.txt";
+            using (var br = File.OpenRead(new DirectoryInfo(".").FullName + path))
+            {
+                // ハッシュアルゴリズム生成
+                var algorithm = new SHA1CryptoServiceProvider();
+
+                // ハッシュ値を計算する
+                var bs = algorithm.ComputeHash(br);
+
+                // リソースを解放する
+                algorithm.Clear();
+
+                // バイト型配列を16進数文字列に変換
+                var result = new StringBuilder();
+                foreach (var b in bs)
+                {
+                    result.Append(b.ToString("X2"));
+                }
+
+                Console.WriteLine(result.ToString());
+            }
         }
     }
 }
